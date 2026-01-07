edda8a31f2b985abba13faa352aa3ff419cd6655
Caio Silva <caio.goncalvesananias@gmail.com>
2026-01-07 11:06:49 -0300

    WIP Msg

--- src/assets/Header.jsx
+++ src/assets/Header.jsx
@@ -2,9 +2,10 @@ import { useImperativeHandle } from "react"
 
 function Header() {
     return (
-        <div className="flex bg-amber-100 w-screen gap-2">
-        <img src="TEMP" alt="TEMP" />
-        <h1 className="text-4xl">Tools 1.0</h1>
+        <div className="flex bg-fuchsia-200 w-screen gap-2">
+            <img className="h-10 w-18
+            " src="src\assets\imgs\undraw_coding_joxb.png" alt="TEMP" />
+            <h1 className="text-4xl">Tools 1.0</h1>
             <a className="ml-auto text-2xl" href="">Olá, user</a>
         </div>
     )
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import Header from './assets/Header.jsx'
+import Wip from './assets/Wip.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Header />
+    <Wip />
   </StrictMode>,
 )

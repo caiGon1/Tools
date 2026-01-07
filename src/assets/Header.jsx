0bf5bc46a1d0aac222f8abd43a40e856c0cdcc94
Caio Silva <caio.goncalvesananias@gmail.com>
2026-01-07 12:48:40 -0300

    fix header

--- src/assets/Header.jsx
+++ src/assets/Header.jsx
@@ -2,11 +2,13 @@ import { useImperativeHandle } from "react"
 
 function Header() {
     return (
-        <div className="flex bg-fuchsia-200 w-screen gap-2">
+        <div className="flex bg-fuchsia-200 gap-2 px-8 py-5">
             <img className="h-10 w-18
             " src="src\assets\imgs\undraw_coding_joxb.png" alt="TEMP" />
             <h1 className="text-4xl">Tools 1.0</h1>
-            <a className="ml-auto text-2xl" href="">Olá, user</a>
+            <a className="ml-auto text-2xl px-1" href="">Olá, user</a>
+            <h1 className="ml-2.5 text-2xl">|</h1>
+            <a className="ml-2.5 text-2xl px-1" href="">Configurações</a>
         </div>
     )
 }
--- src/assets/Wip.jsx
+++ src/assets/Wip.jsx
@@ -1,7 +1,7 @@
 function Wip() {
     return (
         <div className="min-h-screen flex flex-col items-center justify-center">
-        <h1 className="text-5xl">Work In Progress</h1>
+        <h1 className="text-5xl h-32">Work In Progress</h1>
             <img className="w-64 h-64" src="src\assets\imgs\undraw_deep-work_muov.png" alt="Work In Progress" />
         </div>
     )

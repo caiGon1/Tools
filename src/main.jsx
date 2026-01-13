15bf6784dcf317f53428793218f3c1c420d57a7c
Caio Silva <caio.goncalvesananias@gmail.com>
2026-01-13 08:20:14 -0300

    pages structure

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./index.css";
+import "./styles/index.css";
 import Header from "./assets/components/Header.jsx";
 import Wip from "./assets/components/Wip.jsx";
 import Welcome from "./assets/components/Welcome.jsx";

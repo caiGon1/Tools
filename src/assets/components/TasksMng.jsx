188535b226302fd2580e13a79b55fe94e7618344
Caio Silva <caio.goncalvesananias@gmail.com>
2026-01-14 13:22:05 -0300

    wip tsk mng

--- src/assets/components/TasksMng.jsx
+++ src/assets/components/TasksMng.jsx
@@ -1,6 +1,13 @@
 function TasksMng() {
-    return (<div className="h-auto w-auto my-50  bg-pink-400 rounded-2xl flex justify-self-center justify-center ">
-        <h1 className="text-xl align-text-top text-justify-center my-5 italic font-extralight bg-white h-fit rounded-xl ">Gerênciador de Tarefas</h1>
+    return (<div className="h-100 w-150 my-50  bg-pink-400 rounded-md flex-col justify-self-center justify-center ">
+        <h1 className="text-center text-xl text-justify-center my italic h-fit rounded-xl font-bold">Gerênciador de Tarefas</h1>
+        <div className="">
+        <input className="bg-white rounded-md h-10 w-140 mx-5 my-5"placeholder="Digite sua tarefa aqui" type="text" />
+            <input className="bg-white rounded-md h-10 w-140 mx-5 my-5" placeholder="Digite sua descrição aqui" type="text" />
+        </div>
+        <button className="flex bg-white rounded-md h-10 w-30 justify-self-center align italic font-thin">
+            Adicionar
+        </button>
     </div> );
 }
 
